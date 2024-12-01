c7778ce1bced5a9a5ac3e8e28810ca483f4e38e2
Laísa Dacroce <76539042+laisadacroce@users.noreply.github.com>
2024-12-01 13:27:56 -0300

    Update clip.vhd - incompleto

--- clip.vhd
+++ clip.vhd
@@ -1,1 +1,21 @@
+--INCOMPLETO
 
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE ieee.math_real.ALL;
+
+entity clip is
+  port(
+    input : in std_logic_vector(10 downto 0);
+    output : out std_logic_vector(7 downto 0)
+    );
+end clip;
+
+  architecture arch of clip is
+    signal sel_mux1, sel_mux2 : std_logic;
+    signal mux1_in0, mux2_in0, reg_in : std_logic_vector(7 downto 0);
+    begin
+
+      MUX1 : entity work.mux2_1
+      
