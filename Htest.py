a3ab1b43d6ba9ceeba8e7413e497342ac683aa86
jpoanders <jpoanders@gmail.com>
2024-12-03 19:32:56 -0300

    Fix issue in abc_constants

--- Htest.py
+++
@@ -1,8 +0,0 @@
-H_sample = [0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0] #17 samples
-H_sum = 0
-
-for i in range(8):
-    H_sum += (i + 1)*(H_sample[i + 8] - H_sample[6 - i])
-
-H_value = H_sum
-print(H_value)
--- abc_constants.vhd
+++ abc_constants.vhd
@@ -2,26 +2,27 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
-entity abc_constants is
-port(H_value, V_value: in std_logic_vector(14 downto 0);
-    H_byte15, V_byte15: in std_logic_vector(7 downto 0);
-    a: out std_logic_vector(13 downto 0);
-    b, c: out std_logic_vector(10 DOWNTO 0)
-);
-end entity;
+ENTITY abc_constants IS
+	PORT (
+		H_value, V_value : IN STD_LOGIC_VECTOR(14 DOWNTO 0);
+		H_byte15, V_byte15 : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		a : OUT STD_LOGIC_VECTOR(13 DOWNTO 0);
+		b, c : OUT STD_LOGIC_VECTOR(10 DOWNTO 0)
+	);
+END ENTITY;
 
-architecture Behavioral of abc_constants is
-    SIGNAL auxiliar_a: std_logic_vector(12 downto 0);
-    SIGNAL auxiliar_b, auxiliar_c: std_logic_vector(16 downto 0);
-begin
+ARCHITECTURE Behavioral OF abc_constants IS
+	SIGNAL auxiliar_a : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL auxiliar_b, auxiliar_c : STD_LOGIC_VECTOR(16 DOWNTO 0);
+BEGIN
 
-    auxiliar_b <= std_logic_vector(signed(H_value & "00") + resize(signed(H_value), 17) + resize(signed("0100000"), 17));
-    b <= auxiliar_b(16 downto 6);
+	auxiliar_b <= STD_LOGIC_VECTOR(signed(H_value & "00") + resize(signed(H_value), 17) + to_signed(32, 17));
+	b <= auxiliar_b(16 DOWNTO 6);
 
-    auxiliar_c <= std_logic_vector(signed(V_value & "00") + resize(signed(V_value), 17) + resize(signed("0100000"), 17));
-    c <= auxiliar_c(16 downto 6);
+	auxiliar_c <= STD_LOGIC_VECTOR(signed(V_value & "00") + resize(signed(V_value), 17) + to_signed(32, 17));
+	c <= auxiliar_c(16 DOWNTO 6);
 
-    auxiliar_a <= std_logic_vector(resize(unsigned(H_byte15), 9) + resize(unsigned(V_byte15), 9)) & "0000"; -- (H_byte15 + V_byte15) * 16
-    a <= '0' & auxiliar_a; -- 0 concatenated to represent a positive number in 2's complement
+	auxiliar_a <= STD_LOGIC_VECTOR(resize(unsigned(H_byte15), 9) + resize(unsigned(V_byte15), 9)) & "0000"; -- (H_byte15 + V_byte15) * 16
+	a <= '0' & auxiliar_a; -- 0 concatenated to represent a positive number in 2's complement
 
-end Behavioral;
+END Behavioral;
