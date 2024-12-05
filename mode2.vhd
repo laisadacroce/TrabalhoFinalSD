89e91acf6c4f431a9f173eceb17fb2aa0e007d0b
jpoanders <jpoanders@gmail.com>
2024-12-05 09:26:39 -0300

    Change names in mode2

--- mode2.vhd
+++ mode2.vhd
@@ -9,26 +9,26 @@ ENTITY mode2 IS
 		b : POSITIVE
 	);
 	PORT (
-		clk, csample, cdc, cacc, sel_acc, reset : IN STD_LOGIC;
+		clk, cm2, cdc, cacc, sel_acc, reset : IN STD_LOGIC;
 		sample : IN STD_LOGIC_VECTOR(width * b - 1 DOWNTO 0);
 		dc : OUT STD_LOGIC_VECTOR(b - 1 DOWNTO 0)
 	);
 END mode2;
 
 ARCHITECTURE arch OF mode2 IS
-	SIGNAL reg_sample_out : STD_LOGIC_VECTOR(width * b - 1 DOWNTO 0);
+	SIGNAL m2_reg_out : STD_LOGIC_VECTOR(width * b - 1 DOWNTO 0);
 	SIGNAL adder_tree_out : STD_LOGIC_VECTOR(b + POSITIVE(ceil(log2(width))) - 1 DOWNTO 0);
 	SIGNAL acc_in, acc_out : STD_LOGIC_VECTOR(b + POSITIVE(ceil(log2(width))) DOWNTO 0);
-	SIGNAL reg_dc_in : STD_LOGIC_VECTOR(b - 1 DOWNTO 0);
+	SIGNAL dc_reg_in : STD_LOGIC_VECTOR(b - 1 DOWNTO 0);
 
 BEGIN
-	REG_SAMPLE : ENTITY work.reg
+	M2_REG : ENTITY work.reg
 		GENERIC MAP(width => width * b)
-		PORT MAP(clk, csample, reset, sample, reg_sample_out);
+		PORT MAP(clk, cm2, reset, sample, m2_reg_out);
 
-	REG_DC : ENTITY work.reg
+	DC_REG : ENTITY work.reg
 		GENERIC MAP(width => b)
-		PORT MAP(clk, cdc, reset, reg_dc_in, dc);
+		PORT MAP(clk, cdc, reset, dc_reg_in, dc);
 
 	ACC : ENTITY work.accumulator
 		GENERIC MAP(width => b + POSITIVE(ceil(log2(width))) + 1)
@@ -41,6 +41,6 @@ BEGIN
 			is_unsigned => true)
 		PORT MAP(sample, adder_tree_out);
 
-	reg_dc_in <= acc_out(b + POSITIVE(ceil(log2(width))) DOWNTO POSITIVE(ceil(log2(width)))); --Divisão por 32 (desloca 5 bits para a direita)
+	dc_reg_in <= acc_out(b + POSITIVE(ceil(log2(width))) DOWNTO POSITIVE(ceil(log2(width)))); --Divisão por 32 (desloca 5 bits para a direita)
 
 	END arch;
