f4cee6ffea39156049129d18cde55c966801f130
Laísa Dacroce <76539042+laisadacroce@users.noreply.github.com>
2024-12-10 10:22:59 -0300

    Update adder_unsigned.vhd - alterações pra compilar

--- adder_unsigned.vhd
+++ adder_unsigned.vhd
@@ -5,14 +5,14 @@ USE ieee.numeric_std.ALL;
 ENTITY adder_unsigned IS 
     GENERIC (width : INTEGER);
     PORT (
-        A, B : IN unsigned(N - 1 DOWNTO 0);
-        result : OUT std_logic_vector(N - 1 DOWNTO 0)
+        A, B : IN unsigned(width - 1 DOWNTO 0);
+        result : OUT std_logic_vector(width - 1 DOWNTO 0)
     );
 END adder_unsigned;
 
 ARCHITECTURE arch OF adder_unsigned IS
-    SIGNAL sum : unsigned(N DOWNTO 0);
+    SIGNAL sum : unsigned(width DOWNTO 0);
 BEGIN
-    sum <= resize(A, N + 1) + resize(B, N + 1);
-    result <= std_logic_vector(sum(N - 1 DOWNTO 0)); --desconsidera Cout do resultado da soma
+    sum <= resize(A, width + 1) + resize(B, width + 1);
+    result <= std_logic_vector(sum(width - 1 DOWNTO 0)); --desconsidera Cout do resultado da soma
 END arch;
